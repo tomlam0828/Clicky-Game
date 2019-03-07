7ff4694dd5cad1c85f2b1c653e5cd5daf0c82f1e
Tom Lam <tomlam0828@gmail.com>
2019-03-06 19:26:11 -0800

    Updates

---
+++ precache-manifest.b6ed5c46a8871e55768404647621b1df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "3694474c0c4330fe370e",
+    "url": "/Clicky-Game/static/js/main.3694474c.chunk.js"
+  },
+  {
+    "revision": "9faaf100b571f1d79c3b",
+    "url": "/Clicky-Game/static/js/2.9faaf100.chunk.js"
+  },
+  {
+    "revision": "3694474c0c4330fe370e",
+    "url": "/Clicky-Game/static/css/main.1d1a82f9.chunk.css"
+  },
+  {
+    "revision": "37ff00e15a7b24486f52ff8b8ee4fd10",
+    "url": "/Clicky-Game/index.html"
+  }
+];
